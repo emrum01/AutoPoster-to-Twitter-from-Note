07051bfd77c067aad023fcfe468beaf677f4d318
emrum01 <emrum01@gmail.com>
2021-09-13 08:29:30 +0900

    環境変数を追加

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from os.path import dirname, join
 from dotenv import load_dotenv
 
-load_dotenv(verbose=True)
+load_dotenv(".env", verbose=True)
 
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
@@ -10,3 +10,5 @@ load_dotenv(dotenv_path)
 API_KEY = os.environ.get("API_KEY")
 API_KEY_SECRET = os.environ.get("API_KEY_SECRET")
 BEARER_TOKEN = os.environ.get("BEARER_TOKEN")
+ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN")
+ACCESS_TOKEN_SECRET = os.environ.get("ACCESS_TOKEN_SECRET")
