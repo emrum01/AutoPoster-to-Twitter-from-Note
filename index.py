540d9c21e805bce9cfa2a06d368b310d3e373e21
emrum01 <emrum01@gmail.com>
2021-09-13 10:24:01 +0900

    feat: 文単位でツイート

--- index.py
+++ index.py
@@ -4,35 +4,49 @@ import tweepy
 
 TWEET_NUM = 140
 
-url = 'https://note.com/urouro_tk/n/n53cad04b3ec1'
+url = 'https://note.com/urouro_tk/n/n800d2b42e21c'
 article = Article(url)
 article.download()
 article.parse()
-title = article.title.split('｜')[0]
+
+
+def time2Read(length):
+    LETTERS_READ_IN_A_MIN = 600
+    if length % LETTERS_READ_IN_A_MIN > 0.5:
+        return int(length/LETTERS_READ_IN_A_MIN) + 1
+    else:
+        return int(length/LETTERS_READ_IN_A_MIN)
+
 
 articleLength = len(article.text)
-
-# ツイートの回数を計算
-tweetTimes = 1
-tweetTimes += int(articleLength/140)
-if articleLength % 140 > 0:
-    tweetTimes += 1
-
-letterNum = 0
-start = 0
-end = 0
 tw = []
-
+title = article.title.split('｜')[0]
 tw.append(
-    '胡乱なるウーロン茶\n@urouro_tk\nで公開しているショートショートの１つ、「{}」です。\nnoteでも無料公開しています。\n800字程ですのでおよそ2分程で読めると思います。'.format(title))
-for i in range(tweetTimes):
-    if len(article.text) > 140:
-        tw.append(article.text[letterNum:TWEET_NUM])
-        letterNum += 140
-        TWEET_NUM += 140
-    else:
-        tw.append(article.text)
-    print(tw[i])
+    '胡乱なるウーロン茶\n@urouro_tk\nで公開しているショートショートの１つ、「{}」です。\nnoteでも無料公開しています。\n{}字程ですのでおよそ{}分程で読めると思います。'.format(title, articleLength, time2Read(articleLength)))
+articleSentences = article.text.split('。')
+oneTweet = ''
+sentencesInATweet = []
+start = 0
+i = 0
+numArticleSentence = len(articleSentences)
+# 1ツイート分の内容を作成
+
+while i < numArticleSentence:
+    i = start
+    removeCount = 0
+    while len(oneTweet) < 140 and i < numArticleSentence:
+        oneTweet += articleSentences[i]+'。'
+        i += 1
+
+    sentencesInATweet = oneTweet.split('。')
+    while len(oneTweet) > 140:
+        del sentencesInATweet[-1]
+        oneTweet = '。'.join(sentencesInATweet)
+        removeCount += 1
+    tw.append(oneTweet)
+
+    oneTweet = ''
+    start = i - removeCount + 1
 
 # 取得した各種キーを格納-----------------------------------------------------
 consumer_key = config.API_KEY
@@ -46,23 +60,15 @@ api = tweepy.API(auth)
 # -------------------------------------------------------------------------
 
 
-# # ツイートを投稿
-for i in range(tweetTimes):
-    if i == 0:
-        status = api.update_status(tw[i])
-    else:
-        status = api.update_status(tw[i], status.id_str)
+# ツイートの回数を計算
 
-# Account = "@tacubaya_"  # 取得したいユーザーのユーザーIDを代入
-# tweets = api.user_timeline(Account, count=20, page=1)
-# num = 1  # ツイート数を計算するための変数
-# for tweet in tweets:
-#     print('twid : ', tweet.id)               # tweetのID
-#     print('user : ', tweet.user.screen_name)  # ユーザー名
-#     print('date : ', tweet.created_at)      # 呟いた日時
-#     print(tweet.text)                  # ツイート内容
-#     print('favo : ', tweet.favorite_count)  # ツイートのいいね数
-#     print('retw : ', tweet.retweet_count)  # ツイートのリツイート数
-#     print('ツイート数 : ', num)  # ツイート数
-#     print('='*80)  # =を80個表示
-#     num += 1  # ツイート数を計算
+tweetTimes = 1
+tweetTimes += int(articleLength/140)
+if articleLength % 140 > 0:
+    tweetTimes += 1
+# ツイートを投稿
+for j in range(tweetTimes):
+    if j == 0:
+        status = api.update_status(tw[j])
+    else:
+        status = api.update_status(tw[j], status.id_str)
